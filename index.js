27f5d6a10fa5189f3bdea91d7eea49738d6b4414
Sean Wines <sawdev88@gmail.com>
2016-07-31 03:50:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newCats = [...kittens, name];
+  return newCats;
+}
+
+function prependKitten(name){
+  var newCats = [name, ...kittens];
+  return newCats;
+}
+
+function removeLastKitten() {
+  var newCats = kittens.slice(0, 2);
+  return newCats;
+}
+
+function removeFirstKitten() {
+  var newCats = kittens.slice(1,3);
+  return newCats;
+}
