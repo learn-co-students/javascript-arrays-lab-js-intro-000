00f42c7da731df4071130c2dceeba3b7e7a70bc1
Jessica Fischbach <jfisch9@gmail.com>
2016-08-04 02:46:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  // appends a kitten to the end of the kittens array
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  // prepends a kitten to the beginning of the kittens array
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  // removes the last kitten from the kittens array
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  // removes the firs tkitten from the kittens array
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  // appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+  var arr = [...kittens, name];
+  return arr
+}
+
+function prependKitten(name) {
+  // prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+  var arr = [name, ...kittens];
+  return arr;
+}
+
+function removeLastKitten() {
+  // removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged
+  return kittens.slice(0,kittens.length-1);
+}
+
+function removeFirstKitten() {
+  // removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged
+  return kittens.slice(1);
+}
