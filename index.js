30fae6f501a0ac150c2d60a8459dae41480a7e11
Steven Covington <dlinetackle@yahoo.com>
2016-08-05 19:24:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+var kittens =["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return name
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return name
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+  return name
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+  return name
+}
