0f83e808ad648fc549175bff2ad4ad479382f09e
Komediruzec  Dalziher <komediruzecki.2015@gmail.com>
2016-08-08 19:13:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+
+// kittens = ["Mile", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+    kittens.unshift(name)
+  }
+
+function destructivelyRemoveLastKitten(){
+    kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  var newArray = [...kittens, name]
+  return newArray
+}
+
+function prependKitten(name){
+  var newArray = [name, ...kittens]
+  return newArray
+}
+/*
+function removeLastKitten(){
+  var kiten = kittens.slice(0, (kittens.lenght - 1) )
+  return kiten
+}*/
+
+function removeLastKitten(){
+   var newArray = kittens.slice(0, 2)
+   return newArray
+}
+
+function removeFirstKitten(){
+  var newArray = kittens.slice(1, kittens.lenght)
+  return newArray
+}
