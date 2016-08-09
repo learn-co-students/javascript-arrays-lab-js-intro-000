b5108813b6532c63a94b4e818996c0a73e5352ec
Bijan Esnaashari <bijanesnaashari@gmail.com>
2016-08-09 18:51:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(kittens, n){kittens.push(n);return kittens};
