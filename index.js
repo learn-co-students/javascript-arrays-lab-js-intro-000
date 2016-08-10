63c0978382ef17fa3c1bf939309adb440feeeabd
mo6709 <mo6709@gmail.com>
2016-08-10 16:03:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+//Appends a kitten to the end of the kittens array
+function destructivelyAppendKitten(element) {
+	kittens.push(element);
+};
+
+//Prepends a kitten to the beginning of the kittens array
+function destructivelyPrependKitten(element) {
+      kittens.unshift(element);
+};
+
+//Removes the last kitten from the kittens array
+function destructivelyRemoveLastKitten() {
+     kittens.pop();
+};
+
+//Removes the First kitten from the kittens array
+function destructivelyRemoveFirstKitten() {
+   kittens.shift();
+};
+
+//Appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+function appendKitten(name) {
+   var newArray = [...kittens, name];
+   return newArray;
+}; 
+
+//Appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+function prependKitten(name) {
+    var newArray = [name, ...kittens];
+    return newArray;
+};
+ 
+ //Removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged
+ function removeLastKitten() {
+    return kittens.slice(0, kittens.length -1);
+ };
+
+//removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged
+function removeFirstKitten() {
+    return kittens.slice(1);
+};
+
+
+
