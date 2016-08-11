fb395f0a903e5d2a8d674c16ced48358bd87af09
Jessica Fuller <jesfuller@gmail.com>
2016-08-11 19:24:50 +0000

    Done.

--- index.js
+++ index.js
@@ -4,37 +4,30 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
   kittens.push(name);
-  return kittens;
 }
 
 function destructivelyPrependKitten(name) {
   kittens.unshift(name);
-  return kittens;
 }
 
 function destructivelyRemoveLastKitten() {
   kittens.pop();
-  return kittens;
 }
 
 function destructivelyRemoveFirstKitten() {
   kittens.shift();
-  return kittens;
 }
 
 function appendKitten(name) {
   return [...kittens, name];
-  return kittens;
 }
 
 function prependKitten(name) {
   return [name, ...kittens];
-  return kittens;
  }
 
 function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1);
-  return kittens;
 }
 
 function removeFirstKitten() {
