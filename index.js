9bac5be9f46392ea64abef35385fe3646598f8f8
victorwltsang <victorwltsang@gmail.com>
2016-08-12 09:37:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var destructivelyAppendKitten = (name) => kittens.push(name);
+
+var destructivelyPrependKitten = (name) => kittens.unshift(name);
+
+var destructivelyRemoveLastKitten = (name) => kittens.pop(name);
+
+var destructivelyRemoveFirstKitten = (name) => kittens.shift(name);
+
+var appendKitten = (name) => [...kittens, name];
+
+var prependKitten = (name) => [name,...kittens];
+
+var removeFirstKitten = () => kittens.slice(1);
+
+var removeLastKitten = () => kittens.slice(0,kittens.length-1);
