59cfbde447ac81726dd297667346722628af929d
Janine Rosen <janinemrosen@gmail.com>
2016-08-16 19:11:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const app = "I don't do much."
 
-function destructivelyAppendKitten(array, name){
-  array.push(name);
-  return array;
+function destructivelyAppendKitten(kittens){
+  returns kittens.push
 }
