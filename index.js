fe8bea031b7271347481a6d8a356df02a08f4038
Supui Lam <supuilam@gmail.com>
2016-08-20 01:27:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+
+var destructivelyAppendKitten =(name)=> {
+  kittens.push(name);
+}
+
+var destructivelyPrependKitten =(name)=> {
+  kittens.unshift(name);
+}
+
+var destructivelyRemoveLastKitten =()=> {
+  kittens.pop();
+  return kittens;
+}
+
+var destructivelyRemoveFirstKitten =()=> {
+  kittens.shift();
+  return kittens;
+}
+
+var appendKitten =(name)=> {
+  return [...kittens, name];
+}
+
+var prependKitten =(name)=> {
+  return [name, ...kittens];
+}
+
+var removeLastKitten =()=> {
+  return kittens.slice(0, -1);
+}
+
+var removeFirstKitten =()=> {
+  return kittens.slice(1);
+}
