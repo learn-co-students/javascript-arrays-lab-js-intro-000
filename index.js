d1965444fad9fd0c51da277d37236203f555da14
Ray Mulligan <rmullig2@optonline.net>
2016-08-22 03:04:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var new_array = [...kittens, name];
+  return new_array
+}
+
+function prependKitten(name) {
+  var kitten = [name, ...kittens];
+  return kitten
+}
+
+function removeLastKitten() {
+  var kitten = kittens.slice(0, kittens.length - 1);
+  return kitten
+}
+
+function removeFirstKitten() {
+  var kitten = kittens.slice(1);
+  return kitten
+}
