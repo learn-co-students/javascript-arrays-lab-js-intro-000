e4f64de381ac3d56967b0fe5f8804aa20eb63faf
David Ganthier <david.ganthier@gmail.com>
2016-08-25 22:16:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+//var kittens = ["Milo", "Otis", "Garfield"];
+
+// Appends a kitten to the end of the kittens array.
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+// Prepends a kitten to the beginning of the kittens array.
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+// Removes the last kitten from the kittens array.
+function destructivelyRemoveLastKitten() {
+  kittens.pop(name);
+  return kittens;
+}
+
+// Removes the first kitten from the kittens array.
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(name);
+  return kittens;
+}
+
+// Adds a kitten to the kittens array and returns a new array.
+// Leaving the kittens array unchanged.
+function appendKitten(name) {
+  var newArray = kittens.push(name);
+  return newArray;
+}
