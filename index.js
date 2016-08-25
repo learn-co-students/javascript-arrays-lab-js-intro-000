4d47679b32ab5d98bc9a240d0264d0c848f96740
Sally Whang <wsaehee21@outlook.com>
2016-08-25 06:31:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  return window.kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return window.kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  return window.kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return window.kittens.shift();
+}
+
+function appendKitten(name){
+  var addKitten = [...window.kittens];
+  addKitten.push(name);
+  return addKitten;
+}
+
+function prependKitten(name){
+  var addKitten = [...window.kittens];
+  addKitten.unshift(name);
+  return addKitten;
+}
+
+function removeLastKitten(){
+  var removeKitten = [...window.kittens];
+  removeKitten.pop();
+  return removeKitten;
+}
+
+function removeFirstKitten(){
+  var removeKitten = [...window.kittens];
+  removeKitten.shift();
+  return removeKitten;
+}
