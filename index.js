c79c194fb0d3baa50a89e61f145f91d3f12afe0d
David Ganthier <david.ganthier@gmail.com>
2016-08-26 22:05:56 +0000

    Done.

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const app = "I don't do much."
 
 //var kittens = ["Milo", "Otis", "Garfield"];
 
-// Appends a kitten to the end of the kittens array.
+// Appends(end) a kitten to the end of the kittens array.
 function destructivelyAppendKitten(name) {
   kittens.push(name);
   return kittens;
 }
 
-// Prepends a kitten to the beginning of the kittens array.
+// Prepends(beginning) a kitten to the beginning of the kittens array.
 function destructivelyPrependKitten(name) {
   kittens.unshift(name);
   return kittens;
@@ -16,19 +16,40 @@ function destructivelyPrependKitten(name) {
 
 // Removes the last kitten from the kittens array.
 function destructivelyRemoveLastKitten() {
-  kittens.pop(name);
+  kittens.pop();
   return kittens;
 }
 
 // Removes the first kitten from the kittens array.
 function destructivelyRemoveFirstKitten() {
-  kittens.shift(name);
+  kittens.shift();
   return kittens;
 }
 
-// Adds a kitten to the kittens array and returns a new array.
+// Appends(end) a kitten to the kittens array and returns a new array.
 // Leaving the kittens array unchanged.
 function appendKitten(name) {
-  var newArray = kittens.push(name);
-  return newArray;
+  var lastKitten = [...kittens, name];
+  return lastKitten;
+}
+
+// Prepends(beginning) a kitten to the kittens array and returns a new array.
+// Leaving the kittens array unchanged.
+function prependKitten(name) {
+  var firstKitten = [name, ...kittens];
+  return firstKitten;
+}
+
+// Remove the last kitten in the kittens array and returns a new array,
+// leaving the kittens array unchanged.
+function removeLastKitten() {
+  var lastKitten = kittens.slice(0, kittens.length - 1);
+  return lastKitten;
+}
+
+// Remove the first kitten from the kittens array and return a new array,
+// leaving the kittens array unchange.
+function removeFirstKitten() {
+  var firstKitten = [...kittens.slice(1)];
+  return firstKitten;
 }
