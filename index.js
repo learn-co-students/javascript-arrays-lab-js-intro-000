2b9109d7bfa9a869a3eb123baf57dc23c88956d8
imani fecu <imani.fecu72@ymail.com>
2016-08-29 03:13:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+  var news = [...kittens, name]
+  return news
+}
+function prependKitten(name){
+  var news = [name, ...kittens]
+  return news
+}
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
