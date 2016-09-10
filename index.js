550f48aa03518c175bd73b3a9322a9e0572903ba
Devy Js <devyjs77@gmail.com>
2016-09-10 19:56:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten)
+  return destructivelyAppendKitten
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten)
+  return destructivelyPrependKitten
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return destructivelyRemoveLastKitten
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return destructivelyRemoveFirstKitten
+}
+
+function appendKitten(kitten) {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens.push(kitten)
+  return kittens
+}
+
+function prependKitten(kitten) {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens.unshift(kitten)
+  return kittens
+}
+
+function removeLastKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens.pop()
+  return kittens
+}
+
+function removeFirstKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens.shift()
+  return kittens
+}
