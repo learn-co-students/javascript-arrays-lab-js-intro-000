ddfe3003bef6e91572a7496d654d4456e5921ae9
crisam degracia <crisamdegracia@gmail.com>
2016-09-15 02:07:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push("Ralph")
+    return kittens
+}
+
+function destructivelyPrependKitten(name){
+
+  kittens.unshift("Bob")
+return kittens
+}
+
+function destructivelyRemoveLastKitten()
+{
+
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten()
+{
+
+  kittens.shift()
+  return kittens
+}
+
+
+function appendKitten(name)
+{
+
+ const xx = ["Milo","Otis","Garfield"]
+ xx.push("Broom")
+ return xx
+
+}
+
+function prependKitten(name){
+
+  const xx = ["Milo", "Otis", "Garfield"]
+  xx.unshift("Arnold")
+  return xx
+}
+
+function removeLastKitten()
+ {
+var xx = ["Milo", "Otis", "Garfield"]
+xx.pop()
+return xx
+}
+
+function removeFirstKitten() {
+var xx =  ["Milo", "Otis", "Garfield"]
+xx.shift()
+return xx
+
+}
