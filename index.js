be917e4536de3c73d69660b8edfd57d2e51db177
Ben Fletcher <bfletch@gmail.com>
2016-09-19 15:18:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var destructivelyAppendKitten = (name) => kittens.push(name);
+
+var destructivelyPrependKitten = (name) => kittens.unshift(name);
+
+var destructivelyRemoveFirstKitten = () => kittens.shift();
+
+var destructivelyRemoveLastKitten = () => kittens.pop();
+
+var appendKitten = (name) => [...kittens, name];
+
+var prependKitten = (name) => [name, ...kittens];
+
+var removeLastKitten = () => kittens.slice(0, -1);
+
+var removeFirstKitten = () => kittens.slice(1, kittens.length);
