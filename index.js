5b1ba73828de07c4677c12debf9013e28e41c4a7
Odi Abdulkareem <odiabdulkareem@gmail.com>
2016-09-28 15:34:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function Arrays() {
+  return kittens;
+}
+
+
+function kittens() {
+  var Kittens = ["Milo", "Otis", "Garfield"];
+  return Kittens;
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(kittens.length-1);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  return kittens;
+}
+
+function prependKitten(name) {
+  kittens= [name,...kittens];
+  return kittens;
+}
+
+function removeLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function removeFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
