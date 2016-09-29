146e3f180c67ec43c31eb702646e699231462a67
David Hollinger <blackmagicarts@gmail.com>
2016-09-29 01:31:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(name) {
+  var kittensCopy = kittens.slice()
+  kittensCopy.push(name)
+  return kittensCopy
+}
+
+function prependKitten(name) {
+  var kittensCopy = kittens.slice()
+  kittensCopy.unshift(name)
+  return kittensCopy
+}
+
+function removeLastKitten() {
+  var kittensCopy = kittens.slice()
+  kittensCopy.pop()
+  return kittensCopy
+}
+
+function removeFirstKitten() {
+  var kittensCopy = kittens.slice()
+  kittensCopy.shift()
+  return kittensCopy
+}
