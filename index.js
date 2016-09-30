07fdf940d6c4975ef3decaa5d8c8050e7dd5d431
Abrahym Sharfeldden <therealsharf@gmail.com>
2016-09-30 13:07:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+var catNames = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name)
+{
+  catNames.push("Ralph")
+  return catNames
+}
+
+function destructivelyPrependKitten(name)
+{
+  catNames.unshift("Bob")
+  return catNames
+}
+
+function destructivelyRemoveLastKitten(name)
+{
+  catNames.shift(1);
+}
+
+function destructivelyRemoveFirstKitten(name)
+{
+  catNames.shift(1,catNames.length)
+}
+
+function appendKitten(name)
+{
+  return [...catNames, "Broom"]
+}
+
+function prependKitten(name)
+{
+  return ["Arnold", ...catNames]
+}
+
+function removeLastKitten(name)
+{
+  catNames.slice(0,1)
+}
+
+function removeFirstKitten(name)
+{
+  catNames.slice(1,2)
+}
