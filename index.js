0a2f49c2482839be2bb74e91a35262acd7b74f07
Ajmal jalal <ajmaljalal58@gmail.com>
2016-10-09 23:48:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const app = "I don't do much."
+
+
+function destructivelyAppendKitten (name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+
+}
+
+function destructivelyRemoveLastKitten (){
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+/*function appendKitten(name){
+  var newArray = kittens ["Milo", "Otis", "Garfield"];
+  newArray.push(name);
+  return newArray;
+}*/
+function prependKitten(name) {
+  kittens.unshift(name);
+
+}
