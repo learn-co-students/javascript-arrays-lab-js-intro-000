e2d8d1dd679cc4c17d493adbab7bd8a355436b41
Krs Lido <krslido@gmail.com>
2016-10-11 17:22:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(kitten){
+  kittens.unshift(kitten);
+}
+
+function destructivelyRemoveLastKitten(kitten){
+  kittens.pop(kitten);
+}
+
+function destructivelyRemoveFirstKitten(kitten) {
+  kittens.shift(kitten)
+}
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ...kittens];
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1);
+}
