a20d5e591b7f6f64f35c5102608900c14ab03f32
Stephen Thompson <stephenthompson91@gmail.com>
2016-10-12 15:54:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten() {
+  return kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten() {
+  return kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(cat) {
+  return [...kittens, cat]
+}
+
+function prependKitten(cat) {
+  return [cat, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
