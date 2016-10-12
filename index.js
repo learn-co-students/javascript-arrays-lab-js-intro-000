78dc4988ee1c843db19e9d536cdb7da5b47e8150
Brent Lewis <blewis1991@live.com>
2016-10-12 05:30:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function kittens(){
+  var kittens = ["Milo", "Otis", "Garfield"];
+}
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+return name;
+}
+
+function destructivelyPrependKitten(name){
+kittens.unshift("Bob");
+return name;
+}
+
+function destructivelyRemoveLastKitten() {
+kittens.pop();
+return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+kittens.shift();
+return kittens;
+}
+
+function appendKitten(name){
+return [...kittens, "Broom"];
+}
+
+function prependKitten(name){
+return ["Arnold", ...kittens];
+}
+
+function removeLastKitten() {
+return kittens.slice(0, 2);
+}
+
+function removeFirstKitten() {
+return kittens.slice(-2);
+}
