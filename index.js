9a584d0d19ed8fa615921594e768a8199cf50b6f
Jonathan Herman <jonathan.n.herman@gmail.com>
2016-10-18 20:38:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return name
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return name
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+function prependKitten(name){
+  var newKittyKitten = [name, ...kittens]
+  return newKittyKitten
+}
+
+function removeLastKitten(){
+  var newCat = kittens.slice(0, kittens.length - 1)
+  return newCat
+}
+
+function removeFirstKitten(){
+  var newKittyCat = kittens.slice(1)
+  return newKittyCat
+}
