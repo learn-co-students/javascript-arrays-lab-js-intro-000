6980dcfdb4054413d6e1eb387893a3c830bbef45
Boris Vasallo <vbl11987@gmail.com>
2016-11-07 20:44:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+};
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+};
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+};
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+};
+
+function appendKitten(name) {
+  var n = [...kittens, name];
+  return n;
+};
+
+function prependKitten(name) {
+  var n = [name, ...kittens];
+  return n;
+};
+
+function removeLastKitten(name) {
+  var n = kittens.slice(0, kittens.length -1);
+  return n;
+};
+
+function removeFirstKitten(name) {
+  var n = kittens.slice(1);
+  return n;
+};
