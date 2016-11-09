200a0cfb93a910695f3e9b8bc2b2375385fbb0ad
Andrew Schubert <andrew.schubert1986@gmail.com>
2016-11-09 04:18:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(name) {
+  var newKittens = [kittens, ...name]
+  return newKittens
+}
+
+function prependKitten(name) {
+  var newKittens = [...name, kittens]
+  return newKittens
+}
+
+function removeLastKitten() {
+  
+}
