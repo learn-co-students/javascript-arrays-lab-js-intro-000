5db68b02e06b8b97b935f952ec35eb0a045ba9ca
Andrew Schubert <andrew.schubert1986@gmail.com>
2016-11-09 04:52:54 +0000

    Done.

--- index.js
+++ index.js
@@ -19,15 +19,21 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var newKittens = [kittens, ...name]
+  var newKittens = [...kittens, name]
   return newKittens
 }
 
 function prependKitten(name) {
-  var newKittens = [...name, kittens]
+  var newKittens = [name, ...kittens]
   return newKittens
 }
 
 function removeLastKitten() {
-  
+  var newKittens = kittens.slice(0, kittens.length - 1)
+  return newKittens
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1)
+  return newKittens
 }
