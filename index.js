dccca997b2b32b67a111bbe9e8b88d16071d7549
Tu An Huynh Van <arishuynhvan@gmail.com>
2016-11-13 04:16:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+function  destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  return kittens.concat([name])
+}
+
+function prependKitten(name){
+  return [name].concat(kittens)
+}
+
+function removeLastKitten(){
+  var tmp=kittens.pop()
+  var rs = kittens.concat()
+  kittens.push(tmp)
+  return rs
+}
+function removeFirstKitten(){
+  var tmp=kittens.shift()
+  var rs = kittens.concat()
+  kittens.unshift(tmp)
+  return rs
+  return
+}
