26979bdfd2fd84e3f96c8def57c8efc3e5dcc2d5
Tu An Huynh Van <arishuynhvan@gmail.com>
2016-11-13 04:17:33 +0000

    Done.

--- index.js
+++ index.js
@@ -32,5 +32,13 @@ function removeFirstKitten(){
   var rs = kittens.concat()
   kittens.unshift(tmp)
   return rs
-  return
+}
+
+//Neater solutions
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
 }
