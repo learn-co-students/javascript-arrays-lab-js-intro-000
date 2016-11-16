a69350bd8006730383a37033f3929c1a57c399d6
michelle vargas <mvargas2193@gmail.com>
2016-11-16 20:35:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+
+  kittens.push(name);
+
+return kittens;
+
+
+}
+
+function destructivelyPrependKitten(name) {
+
+  kittens.unshift(name);
+
+  return kittens;
+
+}
+
+
+function destructivelyRemoveLastKitten() {
+
+  kittens.pop(name);
+
+  return kittens;
+
+
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(name);
+
+  return kittens;
+
+
+}
+
+
+
+function appendKitten(name) {
+
+  var n = [...kittens, name];
+
+  return n;
+
+
+}
+
+function prependKitten(name) {
+
+  var n = [name, ...kittens];
+
+  return n;
+
+
+}
+
+
+function removeLastKitten() {
+
+  return kittens.slice(0, kittens.length -1);
+
+
+}
+
+function removeFirstKitten() {
+
+  return kittens.slice(1);
+
+
+}
