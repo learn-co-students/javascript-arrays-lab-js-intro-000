fbfdfef0395f122b968d61fe67fb3855f95ee484
John Simoni <jsimoni03@gmail.com>
2016-11-16 07:02:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ...kittens];
+}
+
+function removeLastKitten(){
+  var new_arr = [...kittens];
+  new_arr.pop();
+  return new_arr;
+}
+
+function removeFirstKitten(){
+  var new_arr = [...kittens];
+  new_arr.shift();
+  return new_arr;
+}
