ac24858d6e2198abe38a624ed0f7fa59df219686
Muhammad Mehdi <mohummad.mehdi@gmail.com>
2016-11-23 02:35:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+
+}
+
+function appendKitten(name){
+  kitten1=kitten
+  kitten1.push(name)
+  return kitten1
+}
