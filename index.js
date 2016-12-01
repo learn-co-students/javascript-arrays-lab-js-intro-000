1020673210958b89aa55361985b6d1f3748033c0
Ber Cohen <thegreatber@gmail.com>
2016-12-01 19:10:40 +0000

    Done.

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ return name}
 function prependKitten(name){name=["Arnold",...kittens]
 return name}
 
-function removeLastKitten(name){name =[kittens.slice(0, kittens.length-1)]
-  return name
-}
+function removeLastKitten(name){ return kittens.slice(0, kittens.length-1
+)}
+
+function removeFirstKitten(name) {return kittens.slice(1)}
