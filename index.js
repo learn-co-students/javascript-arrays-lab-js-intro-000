52212dad23ff5fb000c1274bae441279f9b936a3
Tim Schmidt <schmidt.tim.j@gmail.com>
2016-12-07 03:22:42 +0000

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,19 @@ function destructivelyRemoveFirstKitten(name){
   kittens.shift(name);
   return kittens;
 }
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ...kittens];
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
