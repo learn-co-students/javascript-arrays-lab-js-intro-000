76b5b0f3372a3114af6417bd1d6649c7d809e015
Jon Roethke <jon@vidroll.com>
2016-12-15 03:04:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+};
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+}
+
+function appendKitten(name) {
+  var kittenz = kittens.slice(0);
+  kittenz.push(name);
+  return kittenz;
+}
+
+function prependKitten(name) {
+  var kittenz = kittens.slice(0);
+  kittenz.unshift(name);
+  return kittenz;
+}
+
+function removeLastKitten(name) {
+  var kittenz = kittens.slice(0);
+  kittenz.pop(name);
+  return kittenz;
+}
+
+function removeFirstKitten(name) {
+  var kittenz = kittens.slice(0);
+  kittenz.shift(name);
+  return kittenz;
+}
