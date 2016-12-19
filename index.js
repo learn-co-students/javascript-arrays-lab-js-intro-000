42fdf405eb198e864c64b923f1573e2ede8afa9a
Ashley C <ashleymcasey@gmail.com>
2016-12-19 00:26:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function kittens() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  return kittens;
+}
+
+function destructivelyAppendKitten(name) {
+  var name = "Ralph";
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  var name = "Bob";
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var name = "Broom";
+  const moreCats = [...kittens, name];
+  return moreCats;
+}
