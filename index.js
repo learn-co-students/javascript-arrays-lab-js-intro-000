260e0707ce4b87dd2d736ed6251603d895871fcd
Ashley C <ashleymcasey@gmail.com>
2016-12-19 00:44:20 +0000

    Done.

--- index.js
+++ index.js
@@ -32,3 +32,21 @@ function appendKitten(name) {
   const moreCats = [...kittens, name];
   return moreCats;
 }
+
+function prependKitten(name) {
+  var name = "Arnold";
+  const moreCats = [name, ...kittens];
+  return moreCats;
+}
+
+function removeFirstKitten() {
+  var removeKittens = kittens;
+  removeKittens = removeKittens.slice(1);
+  return removeKittens;
+}
+
+function removeLastKitten() {
+  var removeKittens = kittens;
+  removeKittens = removeKittens.slice(0, removeKittens.length - 1);
+  return removeKittens;
+}
