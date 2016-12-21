208480ada969ab26f09bed059bbbc1171a391314
Ryan Lee <ryantlee9@gmail.com>
2016-12-21 18:33:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+var removeLastKitten = () => kittens.slice(0, kittens.length - 1)
+var removeFirstKitten = () => kittens.slice(1, kittens.length)
