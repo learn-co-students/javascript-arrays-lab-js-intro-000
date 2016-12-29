d639c328217e5c1b9a6e6e79989f1b8d97c02a87
Salvatore McCarty <salvatore.mccarty@gmail.com>
2016-12-29 01:30:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);  
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  var kittenmod = new Array();
+  kittenmod = [...kittens, name];
+  return kittenmod;
+}
+
+function prependKitten(name) {
+  var kittenmod = new Array();
+  kittenmod = [name, ...kittens];
+  return kittenmod;
+}
+
+function removeLastKitten(){
+  var kittenmod = new Array();
+  kittenmod = kittens;
+  kittenmod.pop();
+}
+
+function removeFirstKitten() {
+  var kittenmod = new Array();
+  kittenmod = kittens;
+  kittenmod.shift();
+}
