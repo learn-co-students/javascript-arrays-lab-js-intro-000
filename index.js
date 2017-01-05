ba60e7b59ce5cf552b5c76b9d82b003f512f0623
Jeremy Wang <jwm191@gmail.com>
2017-01-05 13:07:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+//1
+var kittens = ["Milo", "Otis", "Garfield"];
+
+//2
+function destructivelyAppendKitten (name) {
+  kittens.push(name);
+  return kittens;
+}
+
+//3
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+//4
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+//5
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+//6
+function appendKitten(name) {
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+
+//7
+function prependKitten(name) {
+  var newKittens = [name,...kittens];
+  return newKittens;
+}
+
+//8
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens;
+}
+
+//9
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
