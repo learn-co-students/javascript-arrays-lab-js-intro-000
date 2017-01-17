7e6f874780065dded57e94e1d854c8a71e0d1aca
Edgar Pena <staygftd@gmail.com>
2017-01-17 22:32:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph")
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob")
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+  return [...kittens, name]
+}
+function prependKitten(name){
+  return [name, ...kittens]
+}
+function removeLastKitten(){
+  var kittens = ["Milo", "Otis", "Garfield"];
+  return kittens = kittens.slice(0, kittens.length - 1)
+}
+function removeFirstKitten(){
+  var kittens = ["Milo", "Otis", "Garfield"]
+  return kittens = kittens.slice(1)
+}
