694462ecff175696adacde958c34910b1dd23cf0
zahir surmawala <zahir@me.com>
2017-01-25 06:06:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+
+function appendKitten(name) {
+  var moreKittens = [...kittens, name];
+  return moreKittens;
+}
+
+function prependKitten(name) {
+  var moreKittens = [name, ...kittens];
+  return moreKittens;
+}
+
+function removeLastKitten(name) {
+  var moreKittens = kittens.slice(0, kittens.length -1);
+  return moreKittens;
+}
+
+function removeFirstKitten() {
+  var moreKittens = kittens.slice(1);
+  return moreKittens
+}
