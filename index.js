e4e68775ebc65e39c30b220c8c11c6f3c8473b00
Kaige Lindberg <lindbergkaige@gmail.com>
2017-01-25 13:32:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens = kittens.slice(1)
+}
+
+function appendKitten(name){
+  var morekittens = [...kittens, name]
+  return morekittens
+}
+
+function prependKitten(name){
+  var morekittens = [name, ...kittens]
+  return morekittens
+}
+
+function removeLastKitten(){
+  var morekittens = kittens.slice(0,kittens.length-1)
+  return morekittens
+}
+
+function removeFirstKitten(){
+  var morekittens = kittens.slice(1)
+  return morekittens
+}
