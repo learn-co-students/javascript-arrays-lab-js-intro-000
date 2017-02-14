e26f3ae8183290cb39f3f0b28d55942d3c45ab31
jessica fletcher <cara.kane@gmail.com>
2017-02-14 16:06:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens, (name)];
+}
+
+function prependKitten(name) {
+  return [(name), ...kittens];
+}
+
+function removeLastKitten() {
+  var kittens1;
+  kittens1 = kittens.slice(0, -1);
+  return kittens1;
+}
+
+function removeFirstKitten() {
+  var kittens2
+  kittens2 = kittens.slice(1);
+  return kittens2;
+}
