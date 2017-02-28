0930dd4b7bb5ce40555e09673677412d85b8e046
jay kakadiya <jaykakadiya93@gmail.com>
2017-02-28 00:38:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+kittens.push(name)
+return kittens
+}
+function destructivelyPrependKitten(name){
+kittens.unshift(name)
+return kittens
+}
+function destructivelyRemoveLastKitten(){
+kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name){
+var kitten = ['Milo', 'Otis','Garfield']
+kitten.push(name)
+return kitten
+}
+function prependKitten(name){
+  var kitten = ['Milo', 'Otis','Garfield']
+  kitten.unshift(name)
+  return kitten
+}
+function removeLastKitten(){
+  var kitten = ['Milo', 'Otis', 'Garfiel']
+  kitten.pop()
+  return kitten
+}
+function removeFirstKitten(){
+  var kitten = ['Milo', 'Otis', 'Garfield']
+  kitten.shift()
+  return kitten
+}
