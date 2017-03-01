443543ee5bdab26b30e888977015368e4bf6024c
Na Kyung Lee <ew1415048nk@gmail.com>
2017-03-01 10:00:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  kittens.push(name)
+  var newArr = kittens
+  kittens = kittens.slice(0, kittens.length-1)
+  return newArr
+}
+
+function prependKitten(name){
+  kittens.unshift(name)
+  var newArr = kittens
+  kittens = kittens.slice(1)
+  return newArr
+}
+
+function removeLastKitten(){
+  var tt = kittens.pop()
+  var newArr = kittens
+  kittens = [...kittens,tt]
+  return newArr
+}
+
+function removeFirstKitten(){
+  var tt = kittens.shift()
+  var newArr = kittens
+  kittens = [tt, ...kittens]
+  return newArr
+}
