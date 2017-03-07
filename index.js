ebd5af7419f97ed678e53cb9ea6dbdcb4ccd0442
Jared Hansen <tool4rage@gmail.com>
2017-03-07 09:14:37 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function destructivelyRemoveLastKitten() {
 }
 
 function destructivelyRemoveFirstKitten() {
-  return kittens.shift(name);
+  return kittens.shift();
 }
 
 function appendKitten(name) {
@@ -26,10 +26,10 @@ function prependKitten(name) {
   return [name, ...kittens];
 }
 
-function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1);
+function removeFirstKitten() {
+  return kittens.slice(1);
 }
 
-function removeFirstKitten() {
-  return kittens.slice(1, kittens.length);
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
 }
