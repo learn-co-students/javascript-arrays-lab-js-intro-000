dafd5829b4dbfa0f6a9fe021a4c5bf8763c86a1c
David Mars <david.w.mars@my.occc.edu>
2017-03-10 06:04:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+var kittens = ['Milo','Otis','Garfield'];
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+  return kittens;
+}
+function appendKitten(name){
+  var kek = [...kittens, `${name}`];
+  return kek;
+}
+function prependKitten(name){
+var kek247 = [`${name}`, ...kittens];
+  return kek247;
+}
+function removeLastKitten(){
+  var kittens2 = kittens.slice(0, kittens.length - 1);
+  return kittens2;
+}
+
+function removeFirstKitten() {
+  var kittens1 = kittens.slice(1);
+  return kittens1;
+}
