73f91ba62098f05adc213073b227e04ca3e5d358
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-03-23 21:22:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var newKittens = kittens.concat(name);
+  return newKittens;
+}
+
+function prependKitten(name) {
+  var newKittens = kittens.slice(0);
+  newKittens.unshift(name);
+  return newKittens;
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0);
+  newKittens.pop(name);
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(0);
+  newKittens.shift(name);
+  return newKittens;
+}
