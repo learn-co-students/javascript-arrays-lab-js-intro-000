a6d03763db925bfc8f6f337d508e30c3e79cb528
Dipika Sen <dipika.sen322@gmail.com>
2017-03-26 22:02:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten);
+  return kittens;
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(kitten) {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(kitten) {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens,name];
+}
+
+function prependKitten(name) {
+  return [name,...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
