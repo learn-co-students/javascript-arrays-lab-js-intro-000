7c7f94297a142e6547add406b0ffd6d79cc32086
Mili Upadhyaya Koirala <glady.sharma282@gmail.com>
2017-04-04 02:36:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+
+  kittens.pop(name);
+}
+
+function  destructivelyRemoveFirstKitten(){
+
+  kittens.shift(name);
+}
+
+function appendKitten(name){
+
+  var newArray = [...kittens, name];
+
+  return newArray;
+}
+
+function  prependKitten(name){
+
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+
+ function removeLastKitten(){
+  var kits = [];
+   //non destructive methods
+   kits = kittens.slice(0, kittens.length -1);
+   return kits;
+ }
+
+function removeFirstKitten(){
+  var kits = [];
+  kits = kittens.slice(1);
+  return kits;
+}
