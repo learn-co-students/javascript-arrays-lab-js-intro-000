f7c59ce1cf8b6e8941aae464ec9de344b74a77a1
Kenneth Yung <omkeyu@gmail.com>
2017-04-18 11:41:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+
+function appendKitten(name) {
+  var newkittens = [...kittens,name]
+  return newkittens
+}
+
+function prependKitten(name) {
+  var newkittens = [name,...kittens]
+  return newkittens
+}
+
+function removeLastKitten() {
+  var newkittens = kittens.slice(0,kittens.length-1)
+  return newkittens
+}
+
+function removeFirstKitten() {
+  var newkittens = kittens.slice(1)
+  return newkittens
+}
