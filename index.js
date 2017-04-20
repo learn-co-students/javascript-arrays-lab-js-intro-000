c484946e7594212fe5905b65764e254984ea59f6
Valerie Corvington Farrell <vcorvington@gmail.com>
2017-04-20 23:10:07 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,44 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(kittens, name) {
+function destructivelyAppendKitten(name) {
   kittens.push(name);
   return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(...kittens);
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name,]
+  return newKittens
+}
+
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
+
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, kittens.length -1);
+  return newKittens
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1);
+  return newKittens
 }
