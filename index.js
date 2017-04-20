3f289da3ad24e3e7dd7fbc8a1560bd1c3c2d134e
Pamela Lee <pamela.jean.lee@gmail.com>
2017-04-20 22:48:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function kittens() {
+  return kittens
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var newKittens = kittens.slice()
+  newKittens.push(name)
+  return newKittens
+}
+
+function prependKitten(name) {
+  var newKittens = kittens.slice()
+  newKittens.unshift(name)
+  return newKittens
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice()
+  newKittens.pop(name)
+  return newKittens
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice()
+  newKittens.shift(name)
+  return newKittens
+}
