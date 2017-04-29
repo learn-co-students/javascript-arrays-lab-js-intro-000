b87b22c41d84abf7ab8d695122e67042d90d84f6
Sohee Cho <soheecho94@gmail.com>
2017-04-29 04:36:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens
+}
+
+destructivelyAppendKitten("Ralph");
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+destructivelyPrependKitten("Ralph");
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens
+}
+
+destructivelyRemoveLastKitten("Ralph");
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens
+}
+
+destructivelyRemoveFirstKitten("Ralph");
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+appendKitten("Broom");
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+prependKitten("Broom");
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+removeLastKitten();
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
+
+removeFirstKitten();
