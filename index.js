ab8e128baece1c2d24cff6ae1ebd46115ba80090
Bennie Ghou <b.ghouca@gmail.com>
2017-05-05 22:40:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(cat){
+  kittens.push(cat);
+  return kittens;
+}
+
+function destructivelyPrependKitten(cat){
+  kittens.unshift(cat);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(cat){
+  kittens.pop()
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function prependKitten(cat){
+  return [cat, ...kittens]
+}
+
+function appendKitten(cat){
+  return [...kittens, cat];
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
