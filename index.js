18c14b079a4476b83a7551710e87653866efa3f5
Margaret Hoagland <mehoagland@gmail.com>
2017-05-08 18:58:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+}
+
+function appendKitten(name){
+  var newArray = [...kittens, name];
+  return newArray;
+}
