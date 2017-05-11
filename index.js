a797c5929a928d2016d0031c8d59db635fe12aa2
Dani Schuhman <dani8439@aol.com>
2017-05-11 16:13:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+    kittens.push(name);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyPrependKitten(name) {
+    kittens.unshift(name);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+}
+
+
+function appendKitten(name) {
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+
+function prependKitten(name) {
+    var newArray = [name, ...kittens];
+    return newArray;
+}
+
+function removeLastKitten(name) {
+    var newArray = kittens.slice(0, 2);
+    return newArray;
+}
+
+function removeFirstKitten(name) {
+    var newArray = kittens.slice(1);
+    return newArray;
+}
