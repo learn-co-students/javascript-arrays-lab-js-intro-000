a8a41c3bf0e84006c2568e095554204906d9fbad
Máté Rajnai <ramate.rajnai@gmail.com>
2017-06-01 11:45:11 +0000

    Done.

--- index.js
+++ index.js
@@ -17,6 +17,3 @@ function prependKitten(name) {return [name, ...kittens]}
 function removeLastKitten(name) {return kittens.slice(0, kittens.length-1)}
 
 function removeFirstKitten(name) {return kittens.slice(1)}
-
-
-done()
