3fd4d3712b89e76fe3df918b9c5988ab170039ce
Todd <sanddog42@hotmail.com>
2017-06-05 20:58:26 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+var kittens = ["Milo" , "Otis" , "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var skittens = [...kittens, name];
+  return skittens
+}
+
+
+function prependKitten(name){
+  var skittens = [name, ...kittens];
+  return skittens
+}
+
+function removeLastKitten() {
+  var skittens = kittens.slice(0,-1);
+  return skittens
+}
+
+function removeFirstKitten() {
+  var skittens = kittens.slice(1);
+  return skittens
+}
