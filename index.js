0f4de53e2b43199216fdf8645d78a80a6264437e
Hui Wang <huiwang339@gmail.com>
2017-06-07 19:01:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const app = "I don't do much."
+var kittens =  ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+kittens.pop();
+return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens
+}
+
+function appendKitten(name){
+var newArr = kittens.slice();
+newArr.push(name);
+return newArr;
+}
+
+function prependKitten(name){
+var newArr = kittens.slice();
+newArr.unshift(name);
+return newArr;
+}
+
+function removeLastKitten(name){
+var newArr = kittens.slice();
+newArr.pop();
+return newArr;
+}
+
+function removeFirstKitten(name){
+var newArr = kittens.slice();
+newArr.shift();
+return newArr;
+}
