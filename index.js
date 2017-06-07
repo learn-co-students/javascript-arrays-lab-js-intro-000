d4bbc14be3befe9b263101d23144d32349209175
Matthew Clinton <clintonmatthew@hotmail.com>
2017-06-07 14:51:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+ }
+
+ function destructivelyRemoveLastKitten(){
+   kittens.pop()
+   return kittens
+ }
+
+ function destructivelyRemoveFirstKitten() {
+   kittens.shift()
+   return kittens
+ }
+
+ function appendKitten(name){
+   const morekittens = [...kittens, name]
+   return morekittens
+ }
+
+ function prependKitten(name){
+   const morekittens = [name, ...kittens]
+   return morekittens
+  }
+
+  function removeLastKitten(){
+    return kittens.slice(0, kittens.length - 1)
+  }
+
+  function removeFirstKitten() {
+    return kittens.slice(1)
+  }
