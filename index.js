b863a8a3f8746f2861174f7a769588fb54434543
John Roche <roche.johnjr@gmail.com>
2017-06-15 03:48:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+var whiskers = [...kittens, "Broom"];
+
+function appendKitten(name) {
+  return whiskers
+}
+
+var terminator = ["Arnold", ...kittens];
+
+function prependKitten(name) {
+  return terminator
+}
+
+var knife = kittens.slice(0, kittens.length - 1);
+
+function removeLastKitten() {
+  return knife
+}
+
+var dice = kittens.slice(1);
+
+function removeFirstKitten() {
+  return dice
+}
