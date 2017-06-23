8e379e347a7a9cd1ff51237530b09ff104d222da
Ryan Maurer <ryan_k_maurer@yahoo.com>
2017-06-23 04:29:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+const kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var one = [...kittens, name];
+  return one
+}
+
+function prependKitten(name) {
+  var two = [name, ...kittens];
+  return two
+}
+
+function removeLastKitten() {
+  var three = kittens.slice(0, kittens.length - 1);
+  return three
+}
+
+function removeFirstKitten() {
+  var four = kittens.slice(-2);
+  return four
+}
