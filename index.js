24076beb773f3e999b60215d84fe2f8ac99e4803
Hoi Ki Yuen <cyhk116@gmail.com>
2017-06-24 05:53:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift(1);
+  return kittens;
+}
+
+function appendKitten(name)
+{
+  var n = [...kittens, name];
+  return n;
+}
+
+function prependKitten(name)
+{
+  var n = [name, ...kittens];
+  return n;
+}
+
+function removeLastKitten()
+{
+  var n = kittens.slice(0, kittens.length-1);
+  return n;
+}
+
+function removeFirstKitten()
+{
+  var n = kittens.slice(1, kittens.length);
+  return n;
+}
