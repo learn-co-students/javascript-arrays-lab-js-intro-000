65646126c05f32b673aa7181705ca877fa024d6e
victor wilen <aswilen@gmail.com>
2017-07-21 05:05:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+function appendKitten(name){
+  var NewArray =  [...kittens, name] ;
+  return NewArray;
+}
+function prependKitten(name){
+  var NewArray =  [ name, ...kittens] ;
+  return NewArray;
+}
+ function removeLastKitten(){
+   var NewArray = kittens.slice(0, kittens.length -1);
+   return NewArray;
+ }
+ function removeFirstKitten(){
+   var NewArray = kittens.slice(1);
+   return NewArray;
+ }
