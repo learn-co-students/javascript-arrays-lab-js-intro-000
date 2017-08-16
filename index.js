332f90675bb58dbc82a21ab45bfdb1960ca94f6c
Adam Fisher <ae.fisher.gains@gmail.com>
2017-08-16 15:50:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyRemoveLastKitten() {
+  kittens.pop(2)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(1)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function appendKitten(name) {
+  var newarray = [...kittens, name]
+  return newarray
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function prependKitten(name) {
+  var newarray = [name, ...kittens]
+  return newarray
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function removeLastKitten() {
+  var newarray = kittens.slice (0, kittens.length - 1)
+  return newarray
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function removeFirstKitten() {
+  var newarray = kittens.slice(1)
+  return newarray
+}
