60c1184507e68638f530db71a0f7e8884d598e72
Lois Lee <lois.lee@me.com>
2017-08-17 16:37:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendKitten(){
+   kittens = [...kittens, "Ralph"];
+  return kittens
+}
+
+function destructivelyPrependKitten(){
+   kittens = ["Bob",...kittens];
+  return kittens
+}
+
+
+function destructivelyRemoveLastKitten(){
+   kittens.pop();
+  return kittens
+}
+
+
+function destructivelyRemoveFirstKitten(){
+   kittens.shift();
+  return kittens
+}
+
+function appendKitten(){
+  var newarray=[ ...kittens, "Broom"];
+ return newarray
+}
+
+function prependKitten(){
+  var newarray= [ "Arnold",...kittens];
+ return newarray
+}
+
+function removeLastKitten(){
+  var newarray= kittens.slice(0, kittens.length - 1);
+ return newarray
+}
+
+function removeFirstKitten(){
+  var newarray= kittens.slice(1);
+ return newarray
+}
