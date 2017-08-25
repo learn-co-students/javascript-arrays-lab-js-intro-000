1bad0f5e0bae868e9f804fd49dabda7688ee1a57
Steven Oladipo <stevenboladipo@gmail.com>
2017-08-25 21:48:10 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,32 @@ function destructivelyAppendKitten(name) {
      kittens.push(name);
      return kittens;
      }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name) {
+return kittens.concat(name);
+}
+
+function prependKitten(name) {
+return kittens.unshift();
+
+}
+
+function removeLastKitten() {
+   kittens.pop();
+   return kittens;
+}
