366a90e12ea99b3f7ce9761da6de769b5bb1f7eb
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-08-27 13:08:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+	kittens.push(name)
+	return kittens
+}
+
+function destructivelyPrependKitten(name){
+	kittens.unshift(name)
+	return kittens
+}
+
+function  destructivelyRemoveLastKitten(){
+	kittens.pop()
+	return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+	kittens.shift()
+	return kittens
+}
+
+function appendKitten(name){
+	var cats = kittens.slice()
+	cats.push(name)
+	return cats
+}
+
+function prependKitten(name){
+	var cats = kittens.slice()
+	cats.unshift(name)
+	return cats
+}
+
+function removeLastKitten(){
+	var cats = kittens.slice()
+	cats.pop()
+	return cats
+}
+
+function removeFirstKitten(){
+	var cats = kittens.slice()
+	cats.shift()
+	return cats
+}
