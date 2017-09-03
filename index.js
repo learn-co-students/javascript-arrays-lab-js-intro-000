f9c86c17d00bb9b28fcc3288a3d096cab32d2718
Lisa Mae <lisa@lisapegher.com>
2017-09-03 17:07:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(another_kitten) {
+  kittens.push(another_kitten);
+  return kittens;
+}
+function destructivelyPrependKitten(another_kitten) {
+  kittens.unshift(another_kitten);
+  return kittens;
+}
+function destructivelyRemoveLastKitten(another_kitten) {
+  kittens.pop(another_kitten);
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+function appendKitten(another_kitten){
+  var newKittens = [...kittens, another_kitten];
+  return newKittens;
+
+}
+function prependKitten(another_kitten){
+  var newKittens = [another_kitten, ...kittens];
+  return newKittens;
+}
+function removeLastKitten() {
+  var newKittens = kittens.slice(0,-1);
+  return newKittens;
+}
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
