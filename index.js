2d7da4cbbe22b10d3272ea0dc9c21e4865dc6269
rlrh1996 <rlrh1996@gmail.com>
2017-09-12 09:33:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+    kittens.push(name);
+    // return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+    kittens.unshift(name);
+    // return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+    kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+    kittens.shift();
+}
+
+function appendKitten(name) {
+    var array = [...kittens, name];
+    return array;
+}
+
+function prependKitten(name) {
+    var array = [name, ...kittens];
+    return array;
+}
+
+function removeLastKitten() {
+    var array = kittens.slice(0, -1);
+    return array;
+}
+
+function removeFirstKitten() {
+    var array = kittens.slice(1);
+    return array;
+}
