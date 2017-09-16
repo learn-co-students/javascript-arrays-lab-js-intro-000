5841bfdf9265da24720eed10c43f77c95cc9e9c3
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-09-16 20:28:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+function prependKitten() {
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
