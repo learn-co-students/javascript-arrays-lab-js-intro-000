8d48a85f5451b21fa3c21180ad461eacf3ebeb67
Kathleen Howard <kmaryhoward@gmail.com>
2017-09-21 22:35:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(Catz) {
+  kittens.push(Catz);
+  return kittens
+}
+function destructivelyPrependKitten(Catz) {
+  kittens.unshift(Catz);
+  return kittens
+}
+function destructivelyRemoveLastKitten(Catz) {
+  kittens.pop();
+  return kittens
+}
+function destructivelyRemoveFirstKitten(Catz) {
+  kittens.shift();
+  return kittens
+}
+function appendKitten(Catz) {
+  return [...kittens, Catz]
+}
+function prependKitten(Catz) {
+  return [Catz, ...kittens]
+}
+function removeLastKitten(Catz) {
+  return kittens.slice(0, kittens.length -1)
+}
+function removeFirstKitten(Catz) {
+  return kittens.slice(1)
+}
