47c6b6f8f1328c5d1c64665ad5f6f826c03e7f51
soheecho94 <soheecho94@gmail.com>
2017-09-21 16:13:49 +0900

    Done.

--- index.js
+++ index.js
@@ -5,49 +5,33 @@ function destructivelyAppendKitten(name) {
   return kittens
 }
 
-destructivelyAppendKitten("Ralph");
-
 function destructivelyPrependKitten(name) {
   kittens.unshift(name);
   return kittens
 }
-
-destructivelyPrependKitten("Ralph");
 
 function destructivelyRemoveLastKitten(name) {
   kittens.pop(name);
   return kittens
 }
 
-destructivelyRemoveLastKitten("Ralph");
-
 function destructivelyRemoveFirstKitten(name) {
   kittens.shift(name);
   return kittens
 }
 
-destructivelyRemoveFirstKitten("Ralph");
-
 function appendKitten(name) {
   return [...kittens, name]
 }
-
-appendKitten("Broom");
 
 function prependKitten(name) {
   return [name, ...kittens]
 }
 
-prependKitten("Broom");
-
 function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1)
 }
 
-removeLastKitten();
-
 function removeFirstKitten() {
   return kittens.slice(1);
 }
-
-removeFirstKitten();
