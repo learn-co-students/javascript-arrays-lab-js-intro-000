d04d4c7e9d196428a57e4e5ee069db20927ed00b
Kévin R <kevin.rabesaotra@gmail.com>
2017-10-02 18:42:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  return kittens = kittens.slice(1);
+}
+
+function appendKitten(name){
+  const kitten = [...kittens, name];
+  return kitten;
+}
+function prependKitten(name){
+  const kitten = [name, ...kittens];
+  return kitten;
+}
+
+function removeLastKitten(){
+  const kitten = kittens.slice(0, kittens.length - 1)
+  return kitten;
+}
+function removeFirstKitten(){
+  const kitten = kittens.slice(1);
+  return kitten;
+}
