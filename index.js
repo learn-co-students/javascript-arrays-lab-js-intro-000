55339ffc2416c86accf3945bfadd2759ec1fc1f3
Eric Sherman <esherman9@gmail.com>
2017-10-19 05:30:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name){
   kittens.push(name)
   return kittens
-
 }
 
 function destructivelyPrependKitten(name){
-
+  kittens.unshift(name)
 }
 
 function destructivelyRemoveLastKitten(){
