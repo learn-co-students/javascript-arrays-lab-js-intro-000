37f2736f65f0c465c16d4d9e0719d089151dcea9
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-08 03:37:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(name)  {
+  kittens.pop(2)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(0)
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, "Broom"]
+  return newKittens;
+}
+
+function prependKitten(name)  {
+  var newKittens = ["Arnold", ...kittens]
+  return newKittens;
+}
+
+function removeFirstKitten(name)  {
+  var newKittens = kittens.slice(1)
+  return newKittens;
+}
+
+function removeLastKitten(name) {
+  var newKittens = kittens.slice(0, kittens.length -1)
+  return newKittens;
+}
