28c0447d3965b319678e268029dadf1a4c443b4a
Cory West-Forbes <cwestforbes@gmail.com>
2017-12-05 07:23:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+let kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift(name);
+}
+
+function appendKitten(name){
+  var kittenAppend = [...kittens, name];
+  return kittenAppend;
+}
+
+function prependKitten(name) {
+  var kittenPrepend = [name, ...kittens];
+  return kittenPrepend;
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
