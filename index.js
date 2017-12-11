1bc01d7591121655d955818ac5afd237400ecdd1
Jason Pompon <jasonpompon@gmail.com>
2017-12-11 06:12:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+var destructivelyAppendKitten = name => {
+    return kittens.push(name)
+}
+
+var destructivelyPrependKitten = name => {
+    return kittens.unshift(name)
+}
+
+var destructivelyRemoveLastKitten = () => {
+    return kittens.pop()
+}
+
+var destructivelyRemoveFirstKitten = () => {
+    return kittens.shift()
+}
+
+var appendKitten = name => {
+    return [...kittens, name]
+}
+
+var prependKitten = name => {
+    return [name, ...kittens]
+}
+
+var removeLastKitten = () => {
+    return kittens.slice(0, -1)
+}
+
+var removeFirstKitten = () => {
+    return kittens.slice(1)
+}
