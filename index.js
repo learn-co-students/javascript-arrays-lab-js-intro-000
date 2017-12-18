c584752f5496671d6959309dd1809428f0b220bf
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-18 18:43:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+function appendKitten(name){
+  return [...kittens,name];
+}
+function prependKitten(name){
+  return [name, ...kittens];
+}
+function removeLastKitten(){
+  let arr = [...kittens];
+  arr.pop()
+  return arr;
+}
+function removeFirstKitten(){
+  let arr = [...kittens];
+  arr.shift();
+  return arr;
+}
