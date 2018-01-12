8ee4c5107c358a4c2a5f353eed13d858ba59d62e
Barney Fabian <bafa@tutanota.de>
2018-01-12 16:27:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function appendKitten(name) {
+  return [... kittens, name]
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function prependKitten(name) {
+  return [name, ... kittens]
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
