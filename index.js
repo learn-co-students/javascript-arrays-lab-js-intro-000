21a85e529a593a5f319b0ee01dfb3b747c38b479
Andrew Struck-Marcell <astruckm@yahoo.com>
2018-01-22 18:40:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(kitten) {
+  return kittens.push(kitten);
+}
+
+function destructivelyPrependKitten(kitten) {
+  return kittens.unshift(kitten);
+}
+
+function destructivelyRemoveLastKitten(kitten) {
+  return kittens.pop(kitten);
+}
+
+function destructivelyRemoveFirstKitten(kitten) {
+  return kittens.shift(kitten);
+}
+
+function appendKitten(kitten) {
+  var newKittens = [...kittens, kitten];
+  return newKittens
+}
+
+function prependKitten(kitten) {
+  var newKittens = [kitten, ...kittens];
+  return newKittens
+}
+
+function removeLastKitten(kitten) {
+  var newKittens = kittens.slice(0, kittens.length - 1)
+  return newKittens
+}
+
+function removeFirstKitten(kitten) {
+  var newKittens = kittens.slice(1)
+  return newKittens
+}
