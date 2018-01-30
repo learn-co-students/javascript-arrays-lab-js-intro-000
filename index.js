eaa897509c4d7816ef9afeafe8ce10ff4997c0d5
Simone D <monatru3@hotmail.com>
2018-01-30 07:59:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens=["Milo", "Otis", "Garfield"];
+
+var destructivelyAppendKitten=function(name) {
+  kittens.push(name)
+  return kittens
+};
+
+var destructivelyPrependKitten=function(name) {
+  kittens.unshift(name)
+  return kittens
+};
+
+var destructivelyRemoveLastKitten= ()=> { kittens.pop(); return kittens };
+
+var destructivelyRemoveFirstKitten= ()=> { kittens.shift(); return kittens };
+
+var appendKitten= function(name) {
+  var newArray=kittens.slice(0, kittens.length);
+  newArray.push(name);
+  return newArray;
+}
+
+var prependKitten=function(name) {
+  var newArray=kittens.slice(0, kittens.length);
+  newArray.unshift(name);
+  return newArray;
+};
+
+var removeLastKitten=()=>{
+  var newArray=kittens.slice(0, kittens.length);
+  newArray.pop();
+  return newArray;
+};
+
+var removeFirstKitten=()=>{
+  var newArray=kittens.slice(0, kittens.length);
+  newArray.shift();
+  return newArray;
+};
