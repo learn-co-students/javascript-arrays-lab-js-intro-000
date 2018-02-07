f7fddca9d46f6640919eccd3b40ec6217964a0b5
Jon Exume <jonexume@gmail.com>
2018-02-07 06:03:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+
+  kittens.unshift("Bob");
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+
+  kittens.pop("Garfield");
+  return kittens;
+
+}
+
+function destructivelyRemoveFirstKitten() {
+
+kittens.shift("Milo");
+return kittens;
+}
+
+function appendKitten(name) {
+  var kittens = ["Milo", "Otis", "Garfield"];
+kittens.push("Broom");
+return kittens;
+}
+
+function prependKitten(name) {
+  var kittens = ["Milo", "Otis", "Garfield"];
+kittens.unshift("Arnold");
+return kittens;
+}
+
+function removeLastKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"];
+kittens.pop("Garfield");
+return kittens;
+}
+
+function removeFirstKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"];
+kittens.shift("Milo");
+return kittens;
+}
