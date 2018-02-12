5368672ac5c365f52a1c81905e76a6999c335848
Joe Greiner <jrgreiner115@gmail.com>
2018-02-12 19:05:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+var kittens = ["Milo","Otis","Garfield"]
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+function appendKitten(name) {
+ var addKitten = [...kittens, name]
+return addKitten
+}
+function prependKitten(name) {
+ var addKitten = [name, ...kittens]
+return addKitten
+}
+function removeLastKitten() {
+  var removeKitten = kittens.slice(0, kittens.length -1);
+  return removeKitten
+}
+function removeFirstKitten() {
+  var removeKitten = kittens.slice(1);
+  return removeKitten
+}
