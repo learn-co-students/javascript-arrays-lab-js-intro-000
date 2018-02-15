259a27ba2ce8d83ade4a2edbf5cb8c427a685cde
Juan David Bermudez Celedon <juanbermucele@hotmail.com>
2018-02-15 05:05:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  var arr = [...kittens];
+  arr.pop();
+  return arr;
+}
+
+function removeFirstKitten() {
+  var arr = [...kittens];
+  arr.shift();
+  return arr;
+}
