4e7debd4051a5fabaee596b75095e40674607b62
Mike C <mikecronin92@gmail.com>
2018-04-11 05:18:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop();
+}
+
+
+function appendKitten(name) {
+  let new_kittens = kittens.slice();
+  new_kittens.push(name);
+  return new_kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function prependKitten(name) {
+  let new_kittens = kittens.slice();
+  new_kittens.unshift(name);
+  return new_kittens;
+}
+
+function removeKitten() {
+  let new_kittens = kittens.slice();
+  new_kittens.pop();
+  return new_kittens;
+}
+
+function removeLastKitten() {
+  let new_kittens = kittens.slice();
+  new_kittens.pop();
+  return new_kittens;
+}
+
+function removeFirstKitten(){
+  let new_kittens = kittens.slice();
+  new_kittens.shift();
+  return new_kittens;
+}
