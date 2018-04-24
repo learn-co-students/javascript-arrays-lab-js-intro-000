696c03e2e8584c95eeaa3ef2e6303fa730ad67c8
lmkirkiii@gmail.com Kirk <lmkirkiii@gmail.com>
2018-04-24 00:17:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+
+var kittens = [];
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens
+}
+
+function appendKitten(name){
+  var kitties = [...kittens];
+  kitties.push(name)
+  return kitties
+}
+
+function prependKitten(name){
+  var kitties = [...kittens];
+  kitties.unshift(name);
+  return kitties
+}
+
+function removeLastKitten(name){
+  var kitties = [...kittens];
+  kitties.pop(name);
+  return kitties
+}
+
+function removeFirstKitten(){
+  var kitties = [...kittens];
+  kitties.shift();
+  return kitties
+}
