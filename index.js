ac372970992e2bde812a9b86d5643014654e6eb1
Jonathan Hosein <jonathan.hosein@live.com>
2018-05-24 05:21:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+
+  var newKittens = kittens.push(name);
+  return newKittens;
+}
+
+function destructivelyPrependKitten(name) {
+  var newKittens = kittens.unshift(name);
+
+  return newKittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  var newKittens = kittens.pop();
+
+  return newKittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+
+kittens.shift();
+var newKittens =  kittens
+  return newKittens;
+}
+
+function appendKitten(name) {
+
+
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, kittens.length -1 )
+
+  return newKittens;
+}
+
+function removeFirstKitten() {
+
+  var newKittens = kittens.slice(1);
+
+  return newKittens;
+}
