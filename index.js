69780cf24a3dd1e058aa4bb6e33ab772b8dec631
Brian Cortes <briancorts166@gmail.com>
2018-06-04 23:02:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+var destructivelyAppendKitten = name => {
+
+  kittens.push(name)
+}
+
+var destructivelyPrependKitten = name => {
+
+  kittens.unshift(name)
+}
+
+var destructivelyRemoveLastKitten = name => {
+
+  kittens.pop(name)
+}
+
+var destructivelyRemoveFirstKitten = name => {
+
+  kittens.shift(name)
+}
+
+var appendKitten = name => {
+
+  return [...kittens, name]
+}
+
+var prependKitten = name => {
+
+  return [name, ...kittens]
+}
+
+var removeLastKitten = name => {
+
+  return kittens.slice(0, kittens.length - 1)
+
+}
+
+var removeFirstKitten = name => {
+
+  return kittens.slice(1)
+}
