0b2ebbe650cf07702f3f33bd3dadc45b1361cff1
Erika D'Auria <erika.dauria@gmail.com>
2018-06-04 00:59:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much."
+
+var kittens =['Milo','Otis','Garfield']
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function apendKitten(name){
+  apendKitten = 
+}
