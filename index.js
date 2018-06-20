f8cfa7a697bc5e4d5c208a38d11a6d8d29ff4a45
eggsvonsatan <elbadry.d@gmail.com>
2018-06-20 04:21:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(Ralph) {kittens.push(Ralph); return kittens}
+function destructivelyPrependKitten(Fluffy) {kittens.unshift(Fluffy); return kittens}
+function destructivelyRemoveLastKitten(Phil) {kittens.pop(Phil); return kittens}
+function destructivelyRemoveFirstKitten(Steve) {kittens.shift(Steve); return kittens}
+function appendKitten(Broom) {return [...kittens, Broom]}
+function prependKitten(Harry) {return [Harry, ...kittens]}
+function removeLastKitten() {return kittens.slice(0, kittens.length -1)}
+function removeFirstKitten () {return kittens.slice(1)}
