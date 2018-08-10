8b0837afe578ddedcc9f2adc16899dc4dbaca508
danieldjordan@gmail.com <danieldjordan@gmail.com>
2018-08-10 21:22:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}// Add your functions and code here
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function prependKitten (name) {
+  return [name, ...kittens]
+}
+
+function appendKitten (name) {
+  return [...kittens, name]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
