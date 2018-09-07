bc049e9cd7d41090e64ecaccb14efabbb2e6fa6c
barry.c.schechter@gmail.com <barry.c.schechter@gmail.com>
2018-09-07 00:08:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions and code here
+//var kittens =["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+}
+function appendKitten(name){
+  var kittens_new;
+  kittens_new = [...kittens, name]
+  return kittens_new
+}
+function prependKitten(name){
+  var kittens_new
+  kittens_new = [name, ...kittens]
+  return kittens_new
+}
+function removeLastKitten(name){
+  var kittens_new
+  kittens_new = kittens.slice(0, kittens.length - 1)
+  return kittens_new
+}
+function removeFirstKitten (name){
+  var kittens_new
+  kittens_new = kittens.slice(1)
+  return kittens_new
+}
