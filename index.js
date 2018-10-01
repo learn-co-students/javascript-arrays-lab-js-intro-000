b719f5187cf8cbd7cceb14f82cbaf8fa96566424
Jor Ming Poon <jormpoon@gmail.com>
2018-10-01 20:25:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your functions and code here
+var destructivelyAppendKitten = (name) => {
+  kittens.push(name);
+}
+
+var destructivelyPrependKitten = ( name ) => {
+  kittens.unshift(name);
+}
+
+var destructivelyRemoveLastKitten = () => {
+  kittens.pop();
+}
+
+var destructivelyRemoveFirstKitten =  () => {
+  kittens.shift();
+}
+
+var appendKitten = ( name ) => {
+  return [...kittens, name]
+
+}
+
+var prependKitten = (name) => {
+  return [name , ...kittens]
+}
+
+var removeLastKitten = (name) => {
+  var a = kittens.slice( 0 , 2)
+  return a;
+}
+
+var removeFirstKitten = (name) => {
+ var a = kittens.slice(1, kittens.length)
+ return a
+}
