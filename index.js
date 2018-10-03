92651c1cef1ff2fc5fd248c76d73a665a45c77f2
Kate Rentschler <nkpe@outlook.com>
2018-10-03 00:12:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your functions and code here
+var kittens = [
+  'Milo',
+  'Otis',
+  'Garfield'
+];
+
+var Arrays = [
+  'Milo',
+  'Otis',
+  'Garfield'
+];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var kittensAppended=[...kittens, name];
+  return kittensAppended;
+}
+
+function prependKitten(name){
+  var kittensPrepended=[name, ...kittens];
+  return kittensPrepended;
+}
+
+function removeLastKitten(){
+  var kittensLastRemoved = kittens.slice(0, kittens.length-1);
+  return kittensLastRemoved;
+}
+
+function removeFirstKitten(){
+  var kittensFirstRemoved = kittens.slice(1, kittens.length+1);
+  return kittensFirstRemoved;
+}
