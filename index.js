22c2e221d06ae798cbc9ab9c9a8e2363123d6e0a
Vinit Rai <learner.vinit@gmail.com>
2018-12-12 15:27:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var kittens = [] //define your array here
-
+var kittens;
+kittens = ['Milo','Otis','Garfield'];
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+ kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(){
+kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+kittens.shift();
+}
+function appendKitten(name){
+  var moreKittens = kittens.concat(name);
+  return moreKittens;
+}
+function prependKitten(name){
+  var moreKittens = [name,...kittens];
+  return moreKittens;
+}
+function removeLastKitten(){
+var newkittens = kittens.slice(0,kittens.length-1)  ;
+return newkittens;
+}
+function removeFirstKitten(){
+  var newkittens = kittens.slice(1);
+  return newkittens;
+}
