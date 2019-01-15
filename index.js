626a69e88f39f6cb3b707dcffeadfafeb801e87f
Yingqi Chen <chanwk18@hotmail.com>
2019-01-15 08:22:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+ //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  var kittens2=[...kittens,name];
+  return kittens2;
+}
+
+function prependKitten(name){
+  var kittens2=[name,...kittens];
+  return kittens2;
+}
+
+function removeLastKitten(){
+  var kittens2=kittens.slice(0,2);
+  return kittens2;
+}
+
+function removeFirstKitten(){
+  var kittens2=kittens.slice(1);
+  return kittens2;
+}
