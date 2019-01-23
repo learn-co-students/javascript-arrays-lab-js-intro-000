ca7623ef629534bf4df39b3492f5ad6a90204d02
blithe-database-2858 <amavelamala@gmail.com>
2019-01-23 16:28:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var tempArray = [...kittens, name];
+  return tempArray;
+}
+
+function prependKitten(name) {
+  var tempArray = [name, ...kittens];
+  return tempArray;
+}
+
+function removeLastKitten() {
+  var tempArray = kittens.slice(0, kittens.length-1);
+  return tempArray;
+}
+
+function removeFirstKitten() {
+  var tempArray = kittens.slice(1);
+  return tempArray;
+}
