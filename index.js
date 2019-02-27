1bc84ea9deaedf656d673cc69d8c04b9acbf7a7d
Blake A Long <blake.a.long@gmail.com>
2019-02-27 17:21:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-var kittens = [] //define your array here
-
+var kittens = ["Milo", "Otis", "Garfield"];
+//define your array here
+function destructivelyAppendKitten(Scratchy) {
+kittens.push(Scratchy);
+return kittens;
+}
 // Add your functions and code here
