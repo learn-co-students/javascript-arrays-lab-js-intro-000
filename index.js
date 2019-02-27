36fd5808a2850c77a49698df039f1a8e443cbf7d
Blake A Long <blake.a.long@gmail.com>
2019-02-27 17:34:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var kittens = ["Milo", "Otis", "Garfield"];
 //define your array here
-function destructivelyAppendKitten(Scratchy) {
-kittens.push(Scratchy);
+function destructivelyAppendKitten(name) {
+kittens.push("Ralph");
 return kittens;
 }
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+  return kittens;
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+function appendKitten(name)
 // Add your functions and code here
