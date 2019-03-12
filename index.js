505ca60e2be07644a7f80d33d1974040818889d9
Ben Hancock <benjaminhancock@hotmail.co.uk>
2019-03-12 15:08:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function  destructivelyAppendKitten() {
+kittens.push('Ralph');
+return kittens;
+}
+
+function  destructivelyPrependKitten()  {
+kittens.unshift('Bob');
+return kittens;
+}
+
+function  destructivelyRemoveLastKitten() {
+kittens.pop();
+return  kittens;
+}
+
+function  destructivelyRemoveFirstKitten()  {
+kittens.shift()
+return kittens;
+}
+
+function  appendKitten()  {
+  var newKittens = [...kittens, "Broom"];
+  return newKittens;
+}
+
+function  prependKitten() {
+  var newKittens = ["Arnold", ...kittens];
+  return  newKittens;
+}
+function  removeLastKitten()  {
+  var newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens;
+}
+
+function  removeFirstKitten() {
+  var newKittens  = kittens.slice(1);
+  return newKittens;
+}
