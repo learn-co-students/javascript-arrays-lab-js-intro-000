f40f695d2b8a956bdea0904ec88f1064d4d18b8d
emmanuel antoni <emmanuel.antoni@blackrock.com>
2019-03-14 02:58:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
+ //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten (name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten (name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten (name) {
+  kittens.shift(name)
+}
+
+function appendKitten (name) {
+  var array2 = [name]
+  return kittens.concat(array2)
+}
+
+function prependKitten (name) {
+  var array2 = [name]
+  return array2.concat(kittens)
+}
+
+function removeLastKitten () {
+  var array2 = kittens.slice(0,kittens.length-1)
+  return array2
+}
+
+function removeFirstKitten () {
+  var array2 = kittens.slice(1)
+  return array2
+}
