70f21e266d0181f91fab7cfc253c6bcdb144ea14
Justin Nowicki <justinnowicki@gmail.com>
2019-06-22 22:02:54 +0000

    Done.

--- index.js
+++ index.js
@@ -30,5 +30,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens.pop()
+  return kittens.slice(0, 2)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
 }
