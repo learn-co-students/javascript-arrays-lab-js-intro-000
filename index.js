4f7c851b9b3bc4ceb7fcc394edbde28b4869992c
Chris Hodge <chrishodge87@gmail.com>
2019-06-25 15:51:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+
+kittens.push(name)
+
+
+}
+
+function destructivelyPrependKitten(name){
+
+kittens.unshift(name);
+
+}
+
+function destructivelyRemoveLastKitten(){
+
+kittens.pop()
+
+}
+
+function destructivelyRemoveFirstKitten(){
+
+kittens.shift()
+
+}
+
+function appendKitten(name){
+
+var appendKitten = [...kittens,name]
+
+return(appendKitten)
+
+}
+
+function prependKitten(name){
+  var prependKitten = [name,...kittens]
+
+return(prependKitten)
+}
+
+function removeLastKitten(){
+var byeByeKitten = kittens
+
+return byeByeKitten.slice(0, byeByeKitten.length -1)
+
+
+}
+
+//why can't I use pop!!!!
+
+
+function removeFirstKitten(){
+var helloKitten = kittens
+
+return helloKitten.slice(1)
+
+}
