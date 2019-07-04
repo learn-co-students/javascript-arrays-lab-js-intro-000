ef3230908e33241e10455f770ff85d1396201b17
Theo Malaper <theomalaper.cognez@gmail.com>
2019-07-04 06:26:27 +0000

    Done.

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ function appendKitten(name) {
   return [...kittens, name]
 }
 
-function preprendKitten(name) {
+function prependKitten(name) {
   return [name, ...kittens]
 }
 
-function removeLastKitten(name) {
+function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1)
 }
 
-function removeFirstKitten(name) {
-  return kitten.slice(1)
+function removeFirstKitten() {
+  return kittens.slice(1)
 }
