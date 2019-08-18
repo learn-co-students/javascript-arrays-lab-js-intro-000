b56cf53abb8d9e42bb786b2d63459723f5c558ff
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-18 21:30:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+  return kittens.push('Ralph');
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift('Bob');
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+
+function appendKitten(name){
+  return kittens.concat('Broom');
+}
+
+function prependKitten(name){ //need fix
+  return kittens.concat('Arnold');
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, 2);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1, 3);
+}
