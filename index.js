372eb3473cf61ed2e2820bf1111434cbcee9a28a
Parker Thompson <park.thompson@gmail.com>
2019-10-18 03:25:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-var kittens = [] //define your array here
+var kittens = ["Milo","Otis","Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return kittens.concat(name);
+}
+
+function prependKitten(name) {
+  var newKittens = [name];
+  return newKittens.concat(kittens);
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,2);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
