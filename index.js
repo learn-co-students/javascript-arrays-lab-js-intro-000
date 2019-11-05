a64d9f43a192484e0f4e0892c7969e06ed7f3701
lite-closure-2800 <bri.barden@gmail.com>
2019-11-05 20:50:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+kittens.push(name);
+return kittens
+}
+
+function destructivelyPrependKitten(name){
+kittens.unshift(name);
+return kittens
+}
+function destructivelyRemoveLastKitten(){
+kittens.pop()
+return kittens
+}
+function destructivelyRemoveFirstKitten(){
+kittens.shift()
+return kittens
+}
+function appendKitten(name){
+var newRay = [...kittens,(name)]
+return newRay
+}
+function prependKitten(name){
+var kat = [(name),...kittens]
+return kat
+}
+function removeLastKitten(){
+var kit = kittens.slice(0, kittens.length-1)
+return kit
+}
+function removeFirstKitten(){
+var lip = kittens.slice(1)
+return lip
+}
