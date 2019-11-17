4b77d39ac3d3b8f41fc0023124b04e7391848d64
Chandrashekar YM <shekar.ym@gmail.com>
2019-11-17 07:40:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+}
