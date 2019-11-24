787cca0f8974da67d1dce7e36477a9bd92d056dc
enid svymbersky <enid.bersky@gmail.com>
2019-11-24 02:53:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+  function array [kittens] {
+  function destructivelyAppendKitten(name) {
+  kittens = appendKitten("Ralph");
+  return array;
+  }
+
+}
