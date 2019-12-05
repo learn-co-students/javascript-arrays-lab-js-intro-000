48af652e4b3b17f5f898b38a1d9ee0fc8db9d04f
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-05 03:43:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+// Use .push to add name to end of kittens in function destructivelyAppendKitten()
+function destructivelyAppendKitten(name) {
+  var newAppendKittens = kittens.push(name);
+  return newAppendKittens
+}
+
+// Use .shift to add name to beginning of kittens in function destructivelyPrependKitten()
+
+kittens = ["Milo", "Otis", "Garfield"] // Redefine kittens
+function destructivelyPrependKitten(name) {
+var newPrependKittens = kittens.unshift(name);
+return newPrependKittens
+}
+
+kittens = ["Milo", "Otis", "Garfield"] // Redefine kittens
+function destructivelyRemoveLastKitten(name) {
+var newRemoveLastKittens = kittens.pop(name);
+return newRemoveLastKittens
+}
+
+// Use .shift to destructively remove kitten from front of list.
+
+kittens = ["Milo", "Otis", "Garfield"] // Redefine kittens
+function destructivelyRemoveFirstKitten(name) {
+  var newRemoveFirstKittens = kittens.shift(name);
+  return newRemoveFirstKittens
+}
+
+// Use .concat to add name to end of the list.
+
+kittens = ["Milo", "Otis", "Garfield"] // Redefine kittens
+function appendKitten(name) {
+  var newKittens = kittens.concat(name);
+  return newKittens
+}
+
+// Use .concat to add name to the end of the list kittens.
+
+function prependKitten(name) {
+  var newKittens = [name].concat(kittens);
+  return newKittens
+}
+
+// Use .slice to remove the final name from the list kittens.
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0,kittens.length-1)
+  return newKittens
+}
+
+// Use .slice to remove the first kitten from the kittens array.
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1);
+  return newKittens
+}
