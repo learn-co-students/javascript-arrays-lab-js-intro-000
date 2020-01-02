14dea02f4899ccfa64bf1846a34a0549743fe81e
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-02 20:50:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'];
 
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift(name);
+}
+
+function appendKitten(name) {
+  return kittens.concat(name);
+}
+
+function prependKitten(name) {
+  var newCat = [name];
+  return newCat.concat(kittens);
+}
+
+function removeLastKitten(){
+  return kittens.slice(0,2);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
