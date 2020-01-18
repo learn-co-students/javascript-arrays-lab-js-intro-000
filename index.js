b27406719a2894428a5f0c45b6ad7a765be225fb
Scott Keplinger <skeplin17@gmail.com>
2020-01-18 00:28:25 +0000

    Done.

--- index.js
+++ index.js
@@ -32,5 +32,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens = kittens.slice(0, kittens.length -1)
+  return kittens.slice(0, kittens.length -1)
    }
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
