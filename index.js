654c9b324a52d82c26d2fcc9e358cf4fabc123be
moataz osama <moataz.osama@hotmail.com>
2020-03-20 20:00:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+ kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+    kittens.shift()
+    return kittens
+}
+function appendKitten(name){
+ var array= [...kittens,name]
+return  array
+}
+function prependKitten(name){
+ var array= [name,...kittens]
+return  array
+}
+function removeLastKitten(){
+return kittens.slice(0,-1)
+}
+function removeFirstKitten(){
+return kittens.slice(1)
+}
