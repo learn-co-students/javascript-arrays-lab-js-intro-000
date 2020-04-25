2606f3e74fec693f5aae781305fba729cfd3a4fe
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-25 23:04:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  let cats = kittens.concat(name)
+  return cats
+}
+/*
+concat() is used to merge two or more arrays. It does not modify the original and returns a new array instead
+*/
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
