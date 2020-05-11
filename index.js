b39c689b7e16f43d64f7e80e7a02d3b88d051dc4
Ting Chung <tchung682@gmail.com>
2020-05-11 22:01:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-var kittens = [] //define your array here
+var kittens = [] = ["Milo", "Otis", "Garfield"]//define your array here
 
 // Add your functions and code here
+var destructivelyAppendKitten = function(name){
+  kittens.push(name)
+}
+var destructivelyPrependKitten = function(name){
+  kittens.unshift(name)
+}
+var destructivelyRemoveLastKitten = function(){
+  kittens.pop()
+}
+var destructivelyRemoveFirstKitten = function(){
+  kittens.shift();
+}
+var appendKitten = function(name){
+  return [...kittens, name];
+}
+var prependKitten = function(name){
+  return [name, ...kittens]
+}
+var removeLastKitten = function(){
+  return kittens.slice(0,2)
+}
+var removeFirstKitten = function(){
+  return kittens.slice(1,3)
+}
