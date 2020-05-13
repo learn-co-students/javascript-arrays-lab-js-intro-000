219c672ded12444e231ddc2254068c6dd1e86c63
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-13 09:31:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(kitten){
+  kittens.push(kitten)
+}
+
+function destructivelyPrependKitten(kitten){
+  kittens.unshift(kitten)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  }
+
+function appendKitten(name){
+var newkittens
+ newkittens=[...kittens,name]
+return newkittens
+ }
+
+function prependKitten(name){
+  var newkittens
+  newkittens=[name,...kittens]
+  return newkittens
+}
+
+function removeLastKitten(){
+  var newkittens
+  newkittens=kittens.slice(0,kittens.length-1)
+    return newkittens
+}
+
+function removeFirstKitten(){
+  var newkittens
+  newkittens=kittens.slice(1,kittens.length)
+  return newkittens
+  }
