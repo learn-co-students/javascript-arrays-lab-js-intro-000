a1105d7032e6ec01891618215974ec8b9f3ac054
Kyle Nugent <kln5656@gmail.com>
2020-06-11 17:10:33 +0000

    Done.

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
-  var name = 'Ralph';
-  kittens.push(name)
+  return kittens.push(name)
 
-  return kittens
 }
 
 function destructivelyPrependKitten(name) {
-  var name = 'Bob'
-  kittens.unshift(name)
+  return kittens.unshift(name)
 
-  return kittens
 }
 
 function destructivelyRemoveLastKitten() {
@@ -24,27 +20,19 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var newArray;                   //this is the new array with 'broom' added to the end
-  newArray = [...kittens, 'Broom'] //this method doesn't manipulate the array and you can still return the original array
-
-  return newArray
+  return [...kittens, name] //this method doesn't manipulate the array and you can still return the original array
 }
 
 function prependKitten(name) {
-  var newArray;
-  newArray = ['Arnold', ...kittens]
-
-  return newArray
+  return [name, ...kittens]
 }
 
 function removeLastKitten() {
-  var newArray = kittens.slice(0, kittens.length - 1)   //using the slice method we can create a new array without changing the original.
+  return kittens.slice(0, kittens.length - 1)   //using the slice method we can create a new array without changing the original.
                                                         //this method can slice out any part of the array without mutating the original.
-  return newArray
 }
 
 function removeFirstKitten() {
-  var newArray = kittens.slice(1)    //using the slice method we can create a new array without changing the original.
+  return kittens.slice(1)    //using the slice method we can create a new array without changing the original.
 
-  return newArray
 }
