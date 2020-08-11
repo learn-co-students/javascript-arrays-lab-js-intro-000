79ffb38da30a4d4528f9c91dffa9c95065e37f2a
Ada Huang <ada10huang@gmail.com>
2020-08-11 02:13:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
-var destructivelyAppendKitten = (array, name) => {
-  var name = "Ralph";
+var destructivelyAppendKitten = name => {
   kittens.push(name);
   return kittens
 }
 
-var kittens = ["Milo", "Otis", "Garfield"]
-
-var destructivelyPrependKitten = (array, name) => {
-  var name = "Bob";
+var destructivelyPrependKitten = name => {
   kittens.unshift(name);
   return kittens
 }
 
-var kittens = ["Milo", "Otis", "Garfield"]
-
-var destructivelyRemoveLastKitten = (array, name) => {
+var destructivelyRemoveLastKitten = () => {
   kittens.pop()
   return kittens
 }
 
-var kittens = ["Milo", "Otis", "Garfield"]
-
-var destructivelyRemoveFirstKitten = (array, name) => {
+var destructivelyRemoveFirstKitten = () => {
   kittens.shift()
   return kittens
 }
 
-var kittens = ["Milo", "Otis", "Garfield"]
+var appendKitten = name => {
+  return [...kittens, name]
+}
 
-var appendKitten = (array, name) => {
-  var name = "Broom";
-  return [kittens..., name]
+var prependKitten = name => {
+  return [name, ...kittens]
+}
+
+var removeLastKitten = () => {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+var removeFirstKitten = () => {
+  return kittens.slice(1)
 }
