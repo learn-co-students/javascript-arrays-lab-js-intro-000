3df9b7bd38ed48a6cc63a45e4f0d2e76a5e0b5c9
Ada Huang <ada10huang@gmail.com>
2020-08-11 01:44:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 var destructivelyAppendKitten = (array, name) => {
-  kittens.push(name);
   var name = "Ralph";
+  kittens.push(name);
   return kittens
 }
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
 var destructivelyPrependKitten = (array, name) => {
-  kittens.unshift("Bob");
+  var name = "Bob";
+  kittens.unshift(name);
   return kittens
 }
 
@@ -29,3 +30,7 @@ var destructivelyRemoveFirstKitten = (array, name) => {
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
+var appendKitten = (array, name) => {
+  var name = "Broom";
+  return [kittens..., name]
+}
