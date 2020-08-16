c6f5de54ec28ff17b8d4e3a6ba2dbee1908a86fe
alluring-machine-4767 <bgreiser@gmail.com>
2020-08-16 15:13:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+};
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+};
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  let x = [...kittens, name];
+  return x;
+}
+
+function prependKitten(name) {
+  let x = [name, ...kittens];
+  return x;
+}
+
+function removeLastKitten() {
+  let x = kittens.slice(0, kittens.length-1);
+  return x;
+}
+
+function removeFirstKitten() {
+  let x = kittens.slice(1);
+  return x;
+}
