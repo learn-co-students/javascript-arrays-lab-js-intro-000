c0d0b568aaeda4be899c9654c2976b3793493212
Spartak Belov <job.spartak@gmail.com>
2021-03-18 16:48:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(i){
+  kittens.push(i);
+}
+
+function destructivelyPrependKitten(i){
+  kittens.unshift(i);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(i){
+  var ar = kittens.concat();
+  ar.push(i);
+  return ar;
+}
+
+function prependKitten(i){
+  var ar = kittens.slice();
+  ar.unshift(i);
+  return ar;
+}
+
+function removeLastKitten(){
+  var ar = kittens.concat();
+  ar.pop();
+  return ar;
+}
+
+function removeFirstKitten(){
+  var ar = kittens.slice();
+  ar.shift();
+  return ar;
+}
