a607e6c617f9c4d72e887e20537dc4cb60a40fec
Diego Domínguez Melo <diego_14melo@hotmail.com>
2021-05-06 14:22:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+const destructivelyAppendKitten = name => kittens.push(name)
+
+const destructivelyPrependKitten = name => kittens.unshift(name)
+
+const destructivelyRemoveLastKitten = () => kittens.pop()
+
+const destructivelyRemoveFirstKitten = () => kittens.shift()
+
+const appendKitten = name => {
+  let copy = [...kittens]
+  copy.push(name)
+  return copy
+}
+
+const removeFirstKitten = () => {
+  let copy = [...kittens]
+  copy.shift()
+  return copy
+}
+
+const removeLastKitten = () => {
+  let copy = [...kittens]
+  copy.pop()
+  return copy
+}
+
+const prependKitten = name => {
+  let copy = [...kittens]
+  copy.unshift(name)
+  return copy
+}
